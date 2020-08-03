d0421b78ab061472642c534ac4e57c677f9d3ef9
Aqshola <aqsholafid2@gmail.com>
2020-08-03 10:00:30 +0700

    add react component, modal, card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import SearchBox from './search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardCom from './card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+        input:'',
+        data:'',
+            
+    }
+  }
+  searchChange=(e)=>{
+    this.setState({input: e.target.value})
+  }
+  searchClick=()=>{
+    fetch(`https://api.jikan.moe/v3/search/anime?q=${this.state.input}&page=1`).then(res => res.json())
+      .then(el => 
+        this.setState({
+          data: el.results[1]
+        }))
+    console.log(this.state.data)
+  }
+  render(){
+    return(
+      <div className="content">
+        <div className="searchBox">
+          <SearchBox change={this.searchChange} click={this.searchClick} />
+        </div>
+        <div>
+          <CardCom data={this.state.data}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
