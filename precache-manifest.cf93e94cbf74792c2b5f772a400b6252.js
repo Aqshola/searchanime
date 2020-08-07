8c2cbf37d0891bbb5216bf317af34f1f4e478bd9
Aqshola <aqsholafid2@gmail.com>
2020-08-07 16:25:06 +0700

    Updates

---
+++ precache-manifest.cf93e94cbf74792c2b5f772a400b6252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52013e1f8e453f254568c8d8e5b89859",
+    "url": "/searchanime/index.html"
+  },
+  {
+    "revision": "2c7b78a2c6e8b7e9b8c8",
+    "url": "/searchanime/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "07f8d120d93026b34133",
+    "url": "/searchanime/static/css/main.2c2b7cb6.chunk.css"
+  },
+  {
+    "revision": "2c7b78a2c6e8b7e9b8c8",
+    "url": "/searchanime/static/js/2.4dcbea56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/searchanime/static/js/2.4dcbea56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f8d120d93026b34133",
+    "url": "/searchanime/static/js/main.4657f245.chunk.js"
+  },
+  {
+    "revision": "8326645bda4a91baad77",
+    "url": "/searchanime/static/js/runtime-main.b932844b.js"
+  }
+]);
